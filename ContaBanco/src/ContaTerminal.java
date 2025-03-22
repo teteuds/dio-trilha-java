134ee08b2e797a25b516bced6857d8ffb4dd8469
MateusDias <teu.dias00@gmail.com>
2025-03-22 18:57:58 -0300

    ajute entradas

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //UTILIZAR SCANNER
+        Scanner sc = new Scanner(System.in);
+
+        int numeroConta;
+        String agencia;
+        String nomeCLiente;
+        float saldo;
+       
+        //EXIBIR MSG
+        //OBTER VALORES DIGITADOS
+        System.out.println("Por favor, digite com o número da conta: ");
+        numeroConta = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Por favor, digite a agencia: ");
+        agencia = sc.nextLine();
+
+        System.out.println("Por favor, digite o seu nome: ");
+        nomeCLiente = sc.nextLine();
+
+        System.out.println("Por favor, digite o seu saldo: ");
+        saldo = sc.nextFloat();
+        
+        //EXIBIR MSG CONTA CRIADA
+
+        System.out.println("Olá " + nomeCLiente + ", obrigado por criar uma conta em nosso banco, sua agencia é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já esta disponível para saque.");
     }
 }
