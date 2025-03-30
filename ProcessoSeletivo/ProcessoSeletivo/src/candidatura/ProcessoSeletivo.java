da53ca22774813ec737fac267d8feb40ae1d04d8
MateusDias <teu.dias00@gmail.com>
2025-03-30 09:21:26 -0300

    metodo entrandoEmContato

--- ProcessoSeletivo/ProcessoSeletivo/src/candidatura/ProcessoSeletivo.java
+++ ProcessoSeletivo/ProcessoSeletivo/src/candidatura/ProcessoSeletivo.java
@@ -1,12 +1,42 @@
 package candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         
-       imprimirSelecionados();
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+        for(String candidato: candidatos){
+            entrandoEmContato(candidato);
+        }
 
+    }
+
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuaTentano = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuaTentano = !atendeu;
+            if(continuaTentano){
+                tentativasRealizadas++;
+            }else{
+                System.out.println("CONTATO REALIZADO COM SUCESSO");
+            }
+        }while(continuaTentano && tentativasRealizadas<3);
+
+        if(atendeu){
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + " TENTATIVA. ");
+        }else{
+            System.out.println("NAO CONSEGUIMOS CONTATO COM " + candidato + ", NUMERO MAXIMO DE TENTATIVAS " + tentativasRealizadas + " REALIZADA. ");
+        }
+    }
+
+    //metodo auxiliar
+    static boolean atender(){
+        return new Random().nextInt(3) == 1;
     }
 
     static  void imprimirSelecionados(){
